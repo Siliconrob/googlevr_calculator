bee7c2f09afff7bcde7e082214b3788943692f4f
Robin Michael <Siliconrob@siliconheaven.net>
2023-10-01 22:08:19 -0500

    Fixup

--- data_messages/OTA_HotelAvailNotifRQ.py
+++ data_messages/OTA_HotelAvailNotifRQ.py
@@ -1,4 +1,7 @@
+import datetime
 from dataclasses import dataclass
+
+import pendulum
 from glom import glom
 from pydapper import connect
 from data_messages.LengthOfStay import TimeLengthOfStay
@@ -7,8 +10,10 @@ from data_messages.LengthOfStay import TimeLengthOfStay
 @dataclass
 class OTAHotelAvailNotifRQ:
     externalId: str
-    minLengthOfStay: TimeLengthOfStay
-    maxLengthOfStay: TimeLengthOfStay
+    start: datetime.date
+    end: datetime.date
+    minLengthOfStay: int
+    maxLengthOfStay: int
     available: bool
 
 
@@ -16,27 +21,30 @@ def insert_records(file_input: dict, db_name: str) -> int:
     return load_availability(read_availability(file_input), db_name)
 
 
-def load_availability(rates: list[OTAHotelAvailNotifRQ], db_name: str) -> int:
+def load_availability(availabilities: list[OTAHotelAvailNotifRQ], db_name: str) -> int:
+    if len(availabilities) == 0:
+        return
+
     with connect(db_name) as commands:
         commands.execute(
-            f"create table if not exists OTAHotelAvailNotifRQ (externalId varchar(20), start TEXT, end TEXT, baseAmount DECIMAL(18,6), guestCount int, PRIMARY KEY(internalId, start, end))")
+            f"create table if not exists OTAHotelAvailNotifRQ (externalId varchar(20), start TEXT, end TEXT, minLengthOfStay int, maxLengthOfStay int, available bool, PRIMARY KEY(externalId, start, end))")
         rowcount = commands.execute(
-            f"INSERT INTO OTAHotelAvailNotifRQ (internalId, externalId, start, end, baseAmount, guestCount) values (?internalId?, ?externalId?, ?start?, ?end?, ?baseAmount?, ?guestCount?) ON CONFLICT (internalId, start, end) DO UPDATE SET baseAmount = ?baseAmount?, guestCount = ?guestCount?",
+            f"INSERT INTO OTAHotelAvailNotifRQ (externalId, start, end, minLengthOfStay, maxLengthOfStay, available) values (?externalId?, ?start?, ?end?, ?minLengthOfStay?, ?maxLengthOfStay?, ?available?) ON CONFLICT (externalId, start, end) DO UPDATE SET minLengthOfStay = ?minLengthOfStay?, maxLengthOfStay = ?maxLengthOfStay?, available = ?available?",
             param=[{
-                "internalId": rate.internalId,
-                "externalId": rate.externalId,
-                "start": rate.start.isoformat(),
-                "end": rate.end.isoformat(),
-                "baseAmount": float(rate.baseAmount),
-                "guestCount": rate.guestCount
-            } for rate in rates],
+                "externalId": availability.externalId,
+                "start": availability.start.isoformat(),
+                "end": availability.end.isoformat(),
+                "minLengthOfStay": availability.minLengthOfStay,
+                "maxLengthOfStay": availability.maxLengthOfStay,
+                "available": availability.available,
+            } for availability in availabilities],
         )
         return rowcount
 
 
 def read_availability(file_input: dict) -> list[OTAHotelAvailNotifRQ]:
     if len(file_input.keys()) > 1 or glom(file_input, 'OTA_HotelAvailNotifRQ', default=None) is None:
-        return None
+        return []
 
     external_id = glom(file_input, 'OTA_HotelAvailNotifRQ.AvailStatusMessages.@HotelCode')
     availabilities = []
@@ -50,12 +58,12 @@ def read_availability(file_input: dict) -> list[OTAHotelAvailNotifRQ]:
         min_time = min_time_stay.pop() if len(min_time_stay) > 0 else None
         max_time = max_time_stay.pop() if len(max_time_stay) > 0 else None
 
-        parsed_min_time = TimeLengthOfStay(int(min_time["@Time"]), 'SetMinLOS') if min_time is not None else None
-        parsed_max_time = TimeLengthOfStay(int(max_time["@Time"]), 'SetMaxLOS') if max_time is not None else None
+        parsed_min_time = int(min_time["@Time"]) if min_time is not None else None
+        parsed_max_time = int(max_time["@Time"]) if max_time is not None else None
 
         restriction = glom(avail_status_message, 'RestrictionStatus', default=None)
         status = True if restriction["@Status"] == 'Open' else False
-
-        availabilities.append(OTAHotelAvailNotifRQ(external_id, parsed_min_time, parsed_max_time, status))
+        start, end = pendulum.parse(status_application_control["@Start"]), pendulum.parse(status_application_control["@End"])
+        availabilities.append(OTAHotelAvailNotifRQ(external_id, start, end, parsed_min_time, parsed_max_time, status))
     return availabilities
 
