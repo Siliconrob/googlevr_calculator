ff27e4e1c384730630a3855fa16f1639e7be45f3
Robin Michael <Siliconrob@siliconheaven.net>
2023-10-09 13:27:03 -0500

    Anonymize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -6,8 +8,8 @@ from DataStore import load_db, get_dsn, DB_NAME
 from price_calculator.ComputeFeed import compute_feed_price
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_path", action="store", default="c:/test/gvr_inputs")
-parser.add_argument("--load_db", action="store_true", default=False)
+parser.add_argument("--input_path", action="store", default="./sample_files")
+parser.add_argument("--load_db", action="store_true", default=True)
 parser.add_argument("--start", action="store", default="")
 parser.add_argument("--end", action="store", default="")
 parser.add_argument("--book_date", action="store", default="")
@@ -18,12 +20,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     dsn = get_dsn(DB_NAME)
     if args.load_db:
-        db_load_results = load_db(args.input_path, dsn)
+        db_load_results = load_db(os.path.abspath(args.input_path), dsn)
 
     if args.web_ui is False:
         start = args.start if len(args.start) > 0 else "2024-02-21"
         end = args.end if len(args.end) > 0 else "2024-03-01"
-        external_id = args.external_id if len(args.external_id) > 0 else "orp5b45c10x"
+        external_id = args.external_id if len(args.external_id) > 0 else "rental1"
         book_date = args.book_date if len(args.book_date) > 0 else "2023-10-06"
         calculated_feed_price = compute_feed_price(external_id, start, end, book_date, dsn)
     else:
