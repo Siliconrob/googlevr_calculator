2b1d9553982a5c93e190bed1107a2ba4e654cc1a
Robin Michael <Siliconrob@siliconheaven.net>
2024-01-29 12:58:47 -0600

    Allow for multiple extra guest charges

--- data_messages/ExtraGuestCharges.py
+++ data_messages/ExtraGuestCharges.py
@@ -54,10 +54,10 @@ def create_tables(dsn: str):
             """)
 
 
-def load_extra_charges(extra_guest_charges: ExtraGuestCharges,
+def load_extra_charges(extra_guest_charges: list[ExtraGuestCharges],
                        file_info: FileInfo.FileInfo,
                        file_args: DataHandlers.DataFileArgs) -> FileInfo.FileInfo:
-    if extra_guest_charges is None:
+    if len(extra_guest_charges) == 0:
         return None
 
     new_id = FileInfo.load_file(file_info.file_name, file_args.dsn)
@@ -73,77 +73,84 @@ def load_extra_charges(extra_guest_charges: ExtraGuestCharges,
             where file_id != ?file_id?
             """,
                          param={"file_id": new_id})
-        rowcounts["ExtraGuestCharges"] = commands.execute(f"""
-            INSERT INTO ExtraGuestCharges
-            (
-                external_id,
-                adult_charges,
-                xml_contents,
-                file_id
-            )
-            values
-            (
-                ?external_id?,
-                ?adult_charges?,
-                ?xml_contents?,
-                ?file_id?
-            )
-            """,
-                                                          param={
-                                                              "external_id": extra_guest_charges.external_id,
-                                                              "adult_charges": None if extra_guest_charges.adult_charges is None else float(
-                                                                  extra_guest_charges.adult_charges),
-                                                              "xml_contents": extra_guest_charges.xml_contents,
-                                                              "file_id": new_id
-                                                          })
-        last_id = commands.query_first_or_default(f"""
-            select seq
-            from sqlite_sequence
-            WHERE name = ?table_name?
-            """,
-                                                  param={"table_name": "ExtraGuestCharges"}, model=LastId,
-                                                  default=LastId())
-        rowcounts['stay_dates'] = commands.execute(
-            f"""
-                INSERT INTO ExtraGuestCharges_StayDates (external_id, start, end, days_of_week, file_id, parent_id)
-                values (?external_id?, ?start?, ?end?, ?days_of_week?, ?file_id?, ?parent_id?)
-                ON CONFLICT DO NOTHING
-                """,
-            param=[{
-                "external_id": extra_guest_charges.external_id,
-                "start": None if date_range.start is None else date_range.start.isoformat(),
-                "end": None if date_range.end is None else date_range.end.isoformat(),
-                "days_of_week": date_range.days_of_week,
-                "file_id": new_id,
-                "parent_id": last_id.seq
-            } for date_range in extra_guest_charges.stay_dates]),
 
-    file_info.records = 1
+        rowcounts["ExtraGuestCharges"] = []
+        for extra_guest_charge in extra_guest_charges:
+            add_extra_guest_charge = commands.execute(f"""
+                INSERT INTO ExtraGuestCharges
+                (
+                    external_id,
+                    adult_charges,
+                    xml_contents,
+                    file_id
+                )
+                values
+                (
+                    ?external_id?,
+                    ?adult_charges?,
+                    ?xml_contents?,
+                    ?file_id?
+                )
+                """,
+                                                              param={
+                                                                  "external_id": extra_guest_charge.external_id,
+                                                                  "adult_charges": None if extra_guest_charge.adult_charges is None else float(
+                                                                      extra_guest_charge.adult_charges),
+                                                                  "xml_contents": extra_guest_charge.xml_contents,
+                                                                  "file_id": new_id
+                                                              })
+            last_id = commands.query_first_or_default(f"""
+                select seq
+                from sqlite_sequence
+                WHERE name = ?table_name?
+                """,
+                                                      param={"table_name": "ExtraGuestCharges"}, model=LastId,
+                                                      default=LastId())
+
+            rowcounts['stay_dates'] = commands.execute(
+                f"""
+                    INSERT INTO ExtraGuestCharges_StayDates (external_id, start, end, days_of_week, file_id, parent_id)
+                    values (?external_id?, ?start?, ?end?, ?days_of_week?, ?file_id?, ?parent_id?)
+                    ON CONFLICT DO NOTHING
+                    """,
+                param=[{
+                    "external_id": extra_guest_charge.external_id,
+                    "start": None if date_range.start is None else date_range.start.isoformat(),
+                    "end": None if date_range.end is None else date_range.end.isoformat(),
+                    "days_of_week": date_range.days_of_week,
+                    "file_id": new_id,
+                    "parent_id": last_id.seq
+                } for date_range in extra_guest_charge.stay_dates]),
+
+    file_info.records = len(extra_guest_charges)
     file_info.xml_contents = file_args.file_contents
     return FileInfo.update_file(file_info, file_args.dsn)
 
 
-def read_extra_charges(file_args: DataHandlers.DataFileArgs) -> (ExtraGuestCharges, FileInfo.FileInfo):
+def read_extra_charges(file_args: DataHandlers.DataFileArgs) -> (list[ExtraGuestCharges], FileInfo.FileInfo):
     if (len(file_args.formatted_data.keys()) > 1
             or glom(file_args.formatted_data, 'ExtraGuestCharges', default=None) is None):
-        return None, None
+        return [], None
 
     results = FileInfo.FileInfo(file_args.file_name)
     results.timestamp = FileInfo.get_timestamp(glom(file_args.formatted_data, 'ExtraGuestCharges.@timestamp'))
     results.external_id = get_safe_list(glom(file_args.formatted_data, '**.@hotel_id'))
+
+    extras = []
     extra_guest_charges = glom(file_args.formatted_data, '**.ExtraGuestCharge')
     if len(extra_guest_charges) == 0:
-        return None, None
-    extra_guest_charges = extra_guest_charges.pop()
-    extra_adult_charge = glom(extra_guest_charges, 'AgeBrackets.AdultCharge', default=None)
-    if extra_adult_charge is None:
-        None, None
-    charge_amount = Decimal(extra_adult_charge["@amount"])
-    stay_dates = DateRange.parse_ranges(glom(extra_guest_charges, 'StayDates.DateRange', default=[]))
-    charges_dict = glom(file_args.formatted_data, 'ExtraGuestCharges.HotelExtraGuestCharges')
-    extras = ExtraGuestCharges(results.external_id,
-                               stay_dates,
-                               charge_amount,
-                               xmltodict.unparse({'HotelExtraGuestCharges': charges_dict}))
+        return [], None
+
+    for extra_guest_charge in get_safe_list(extra_guest_charges):
+        extra_adult_charge = glom(extra_guest_charge, 'AgeBrackets.AdultCharge', default=None)
+        if extra_adult_charge is None:
+            [], None
+        charge_amount = Decimal(extra_adult_charge["@amount"])
+        stay_dates = DateRange.parse_ranges(glom(extra_guest_charge, 'StayDates.DateRange', default=[]))
+        charges_dict = glom(file_args.formatted_data, 'ExtraGuestCharges')
+        extras.append(ExtraGuestCharges(results.external_id,
+                                   stay_dates,
+                                   charge_amount,
+                                   xmltodict.unparse({'ExtraGuestCharges': charges_dict})))
 
     return extras, results
